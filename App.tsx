275c19325814670c02f612c157b27398b7582b03
NevesTi <ceelso.7@gmail.com>
2023-06-07 21:24:05 -0300

    revisaoProva

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableHighlight, View, Image } from 'react-native';
+import TitleComponent from './components/title-components';
+import ImageComponent from './components/image-components';
+import BottonComponent from './components/botton-components';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <TitleComponent/>
+     <ImageComponent/>
+     <BottonComponent/>
     </View>
   );
 }
@@ -13,8 +19,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#808080',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingVertical:30,
   },
+
+  
+
+  
+
+ 
+
 });
